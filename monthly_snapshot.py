7e3c94029d1a28558b54ed2daf0e47ebbb5c2c5f
caroliwi <caroline.winther@nav.no>
2024-04-25 14:28:24 +0200

    lagt til sjekk for om denne måneden allerede eksisterer

--- monthly_snapshot.py
+++ monthly_snapshot.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from datetime import datetime
+from datetime import date,datetime
 
 from funksjoner import create_client, get_teamkatalogen_data, write_to_BQ
 
@@ -8,18 +8,29 @@ PROJECT_ID = 'heda-prod-2664'
 SA_KEY_NAME = 'heda-access-key'
 DATASET = 'teamkatalogen'
 def main():
-    # Get data from Teamkatalogen
-    df = get_teamkatalogen_data()
-
-    # Process data
-    df['lastet_dato'] = datetime.today()
-    df.rename(columns={'Område': 'Omraade'}, inplace=True)
 
     # Create BigQuery client
     bq_client = create_client(PROJECT_ID, SA_KEY_NAME)
 
-    # Write data to BigQuery
-    write_to_BQ(client=bq_client, table_name="monthly_snapshot_raw", dframe=df, dataset=DATASET)
+    # Check if this month is already loaded
+    this_month = date.today().replace(day=1)
+    query = f"SELECT distinct lastet_dato FROM `{PROJECT_ID}.{DATASET}.monthly_snapshot_raw`"
+    ld_df = bq_client.query(query).to_dataframe()
+    ld_df['lastet_dato'] = pd.to_datetime(ld_df['lastet_dato'], format='%Y-%m-%d').dt.date
+    if this_month in ld_df.lastet_dato.values:
+        print("month already loaded")
+    else:
+        print("loading month" + str(this_month)
+
+        # Get data from Teamkatalogen
+        df = get_teamkatalogen_data()
+
+        # Process data
+        df['lastet_dato'] = date.today()
+        df.rename(columns={'Område': 'Omraade'}, inplace=True)
+
+        # Write data to BigQuery
+        write_to_BQ(client=bq_client, table_name="monthly_snapshot_raw", dframe=df, dataset=DATASET)
 
 if __name__ == '__main__':
     main()
