43d1b5f6f2c41af97532e470b63d99302503efd3
caroliwi <caroline.winther@nav.no>
2024-05-02 14:42:44 +0200

    utvide airflow-dagen til å oppdatere alle tabeller

--- dags/monthly_update_bq_tables.py
+++ dags/monthly_update_bq_tables.py
@@ -5,10 +5,10 @@ from dataverk_airflow import python_operator
 
 image = "ghcr.io/navikt/dvh-images/airflow-etl-spenn:2024-02-22-e47954f"
 
-dag_name = "monthly_snapshot_run"
+dag_name = "monthly_update_bq_tables"
 default_args = {
     "owner": "heda",
-    "start_date": datetime(2024, 4, 25),
+    "start_date": datetime(2024, 5, 2),
     "depends_on_past": False,
 }
 
@@ -17,7 +17,7 @@ allowlist = ["secretmanager.googleapis.com", "bigquery.googleapis.com", "teamkat
 with DAG(
     dag_name,
     default_args=default_args,
-    schedule_interval="@once",
+    schedule_interval="0 0 1 * *",
 ) as dag:
 
     insert_monthly_snapshot_raw = python_operator(
@@ -30,5 +30,25 @@ with DAG(
         allowlist=allowlist,
         image=image,
     )
+    process_snapshot = python_operator(
+        dag=dag,
+        name="process_snapshot",
+        script_path="process_snapshot.py",
+        repo="navikt/hvor-er-damene",
+        branch="main",
+        #slack_channel="#heda",
+        allowlist=allowlist,
+        image=image,
+    )
+    aggregate = python_operator(
+        dag=dag,
+        name="aggregate",
+        script_path="aggregate.py",
+        repo="navikt/hvor-er-damene",
+        branch="main",
+        #slack_channel="#heda",
+        allowlist=allowlist,
+        image=image,
+    )
 
-insert_monthly_snapshot_raw
+insert_monthly_snapshot_raw >> process_snapshot >> aggregate
