186228287b5417c31512d7c589c61bb568fca9d4
= <=>
2024-05-16 12:25:56 +0000

    fikset på plott fro kvinneandel i rekrutering

--- quarto/rekruttering.py
+++ quarto/rekruttering.py
@@ -1,7 +1,6 @@
 #%%
 import pandas as pd
 import plotly.express as px
-
 colours = ['#8269A2', #Purple-400
         '#3386E0', #blue-400
         '#005B82',#deepblue
@@ -30,36 +29,23 @@ def make_stilling_plot():
     df_stillinger["kvinneandel_ansatt"] = round(df_stillinger["Kvinne_ansatt"]/df_stillinger["Total_ansatt"]*100,2)
     df_stillinger['Søknadsfrist'] = pd.to_datetime(df_stillinger['Søknadsfrist'], format='%d.%m.%Y')
 
-    fig = px.scatter(df_stillinger, x="Søknadsfrist", y=["kvinneandel_søkere", "kvinneandel_ansatt"],
-                     size="Antall stillinger",
-                     hover_data=["Stillingsnummer", "Total_ansatt", "Total_søkere", "Stillingstittel i annonse"],
-                     labels={"value": "Kvinneandel", "variable": ""},
-                     color_discrete_sequence = colours,)
-    return fig
-#%%
-if __name__ == "__main__":
-    #%%
-    [df_søkere, df_ansatt] = _get_stilling_data()
-
-    df_søkere = df_søkere.sort_values(by=["Stillingsnummer", "Total"])
-    df_søkere = df_søkere.drop_duplicates(['Stillingsnummer'], keep='last')
-    df_stillinger = df_søkere.merge(df_ansatt[["Stillingsnummer","Publisering til","Total","Kvinne"]], on = "Stillingsnummer", how = "left", suffixes=('_søkere', '_ansatt'))
-    df_stillinger["Søknadsfrist"].fillna(df_stillinger["Publisering til"], inplace = True)
-    df_stillinger["kvinneandel_søkere"] = round(df_stillinger["Kvinne_søkere"]/df_stillinger["Total_søkere"]*100,2)
-    df_stillinger["kvinneandel_ansatt"] = round(df_stillinger["Kvinne_ansatt"]/df_stillinger["Total_ansatt"]*100,2)
-    df_stillinger['Søknadsfrist'] = pd.to_datetime(df_stillinger['Søknadsfrist'], format='%d.%m.%Y')
-    #%%
-    print(df_stillinger.head(5))
     ## aggregate df_Stillinger to quarterly data for kvinne_Ansatt og totalt_ansatt and create a dataframe 
 
-    df_agg = df_stillinger.groupby(pd.Grouper(key='Søknadsfrist', freq='Q')).agg({'Total_ansatt': 'sum', 'Kvinne_ansatt': 'sum'}).reset_index()
-    df_agg['andel'] = df_agg['Kvinne_ansatt']/df_agg['Total_ansatt']*100
-    df_agg.head()
+    df_agg = df_stillinger.groupby(pd.Grouper(key='Søknadsfrist', freq='Q')).agg({'Total_ansatt': 'sum', 'Kvinne_ansatt': 'sum', 'Total_søkere': 'sum', 'Kvinne_søkere': 'sum'}).reset_index()
+    df_agg['kvinne_andel_ansatt'] = df_agg['Kvinne_ansatt']/df_agg['Total_ansatt']*100
+    df_agg['kvinne_andel_søkere'] = df_agg['Kvinne_søkere']/df_agg['Total_søkere']*100
+    df_agg['Mann_ansatt'] = df_agg['Total_ansatt'] - df_agg['Kvinne_ansatt']
 
-    #%%
-    fig = px.bar(df_agg, x="Søknadsfrist", y=["Kvinne_ansatt", "Total_ansatt"])
+
+    fig = px.bar(df_agg, x="Søknadsfrist", y=["Kvinne_ansatt", "Mann_ansatt"], color_discrete_sequence=colours, labels={"value": "Antall", "Søknadsfrist": "", "variable": ""})
+    #
     ## change this to percentages
-    fig_pers = px.bar(df_agg, x="Søknadsfrist", y=["andel"], barmode='group')
+    fig_pers = px.bar(df_agg, x="Søknadsfrist", y=["kvinne_andel_ansatt", "kvinne_andel_søkere"], barmode='group',color_discrete_sequence=colours, labels={"value": "Kvinneandel", "Søknadsfrist": "", "Variable": ""})
+    return [fig, fig_pers]
+#%%
+if __name__ == "__main__":
+
+    fig, fig_pers = make_stilling_plot()
     fig_pers.show()
 
 
