020a905f9e57ec4fbe0cfbac1cd9bd47e67967df
caroliwi <caroline.winther@nav.no>
2024-08-01 10:34:29 +0200

    oppdaterte pakker

--- pp_charts/undersøkelse.py
+++ pp_charts/undersøkelse.py
@@ -1,56 +1,64 @@
 
 import plotly.express as px
 from bucket_functions import read_heda_bucket
+lys_lilla = '#DEC3FF'
+minty='#43B6A5'
+dus_blå = '#A2AC59'
 
 df = read_heda_bucket(bucket_name = 'undersokelse', file_name = 'spørreundersøkelse_ud_23.csv')
-#df = pd.read_csv("../../GitHub/teamkat-hist/data/Spørre2.csv", sep =";")
 
-#get values in data frame not nan
-svar_utdyp = df[df["Er det noe du vil utdype?"].notna()][['Kjønn','Alder', 'Seksjon', 'Er det noe du vil utdype?']]
 
-colours_NAV = ['#3386E0', #blue-400
-                '#005B82', #Deepblue-500
-                '#6AA379',  # Green-200
-                '#368DA8', #Lightblue-700
-                '#C77300', #Orange-600
-                '#8269A2']#Purple-400
+def kakediagram(df, kjonn, navn, vis_legend):
+    df = df[df['Kjønn'] == kjonn]
+    df = df.sort_values(by = 'variabel')
+    fig = px.pie(values=df['Andel'],
+                 names=df['variabel'],
+                 color_discrete_sequence=[minty,lys_lilla, dus_blå,],
+                 title = kjonn
+    fig.update_layout(
+        font=dict(
+            size=40,
+            color="white"
+        ),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        showlegend=vis_legend
+    )
+    fig.update_traces(insidetextfont=dict(color='white'),
+                      outsidetextfont=dict(color='white'))
+    # save as svg
+    fig.write_image(f"figurer_javazone/{navn}_{kjonn}.svg")
+    #fig.show()
 
-colours_NAV.reverse()
+df = df[df["Kjønn"].isin(["Kvinne", "Mann"])]
+df = df[df["Seksjon"].isin(["utvikling"])]
 
-colours = ['#8269A2', #Purple-400
-        '#3386E0', #blue-400
-        '#005B82',#deepblue
-        '#C77300', #Orange-600
-        '#6AA399', #turkis
-]
+#rollemodeller
+yvalue = 'Jeg savner flere  kvinnelige tekniske rollemodeller i NAV IT'
+df1 = df.groupby(["Kjønn", yvalue]).size().reset_index(name='Antall')
+df1["Andel"] = df1.groupby(["Kjønn"])["Antall"].apply(lambda x: x / x.sum()).reset_index()["Antall"]
+df1 = df1.rename(columns={yvalue: 'variabel'})
 
-def make_andel_bar_chart(df, xvalue = "", yvalue = "Jeg trives i jobben min", facet_color = ""):
-    df = df[df["Kjønn"].isin(["Kvinne", "Mann"])]
-    if facet_color == "":
-        df = df[df["Seksjon"].isin(["utvikling"])]
-        df = df.groupby(["Kjønn",yvalue]).size().reset_index(name='Antall')
-        df["Andel"]=df.groupby(["Kjønn"])["Antall"].apply(lambda x: x / x.sum()).reset_index()["Antall"]
-    else:
-        df = df.groupby(["Kjønn", yvalue, facet_color]).size().reset_index(name='Antall')
-        df["Andel"] = df.groupby(["Kjønn", facet_color])["Antall"].apply(lambda x: x / x.sum())
-    df["Andel"] = (100*df["Andel"]).round(1)
-    #df = df[df[yvalue].isin(["Ja"])]
+#kakediagram(df1, 'Kvinne','rollemodeller_nav', vis_legend = False)
+#kakediagram(df1, 'Mann','rollemodeller_nav', vis_legend = True)
 
-    fig = px.bar(df, x="Kjønn", y="Andel", color=yvalue,
-                 barmode='group',
-                 color_discrete_sequence = colours,
-                 #text = "Andel",
-                 #title= yvalue,
-                 #facet_col=facet_color,
-                 #category_orders={"Alder": ["Under 30", "30+"]},
-                 labels={yvalue: "", "Andel":"Prosent", "Kjønn": ""}
-                 #category_orders= {yvalue: ["Ofte", "Aldri", "Det har skjedd"]},
-                 )
-    fig.update_layout( plot_bgcolor='#C0B2D2',
-                        paper_bgcolor= 'rgba(0,0,0,0)',
-                       font_size = 30,
-                       )
-    fig.show()
+#teknisk kompetanse
+yvalue = 'Jeg har opplevd at min tekniske kompetanse blir undervurdert (av meg selv eller andre)'
+df2 = df.groupby(["Kjønn", yvalue]).size().reset_index(name='Antall')
+df2["Andel"] = df2.groupby(["Kjønn"])["Antall"].apply(lambda x: x / x.sum()).reset_index()["Antall"]
+df2 = df2.rename(columns={yvalue: 'variabel'})
+
+kakediagram(df2, 'Kvinne','teknisk_kompetanse_nav', vis_legend = False)
+#kakediagram(df2, 'Mann','teknisk_kompetanse_nav', vis_legend = True)
+
+#normer_og_forventninger
+yvalue = 'Jeg har følt at jeg har gått i mot normer og forventninger når det gjelder mine studie- og/eller karrierevalg'
+df3 = df.groupby(["Kjønn", yvalue]).size().reset_index(name='Antall')
+df3["Andel"] = df3.groupby(["Kjønn"])["Antall"].apply(lambda x: x / x.sum()).reset_index()["Antall"]
+df3 = df3.rename(columns={yvalue: 'variabel'})
+
+#kakediagram(df3, 'Kvinne','normer_og_forventninger_nav', vis_legend = False)
+#kakediagram(df3, 'Mann','normer_og_forventninger_nav', vis_legend = True)
 
 
 #make_andel_bar_chart(df, yvalue = 'Jeg trives i jobben min')
@@ -58,7 +66,7 @@ def make_andel_bar_chart(df, xvalue = "", yvalue = "Jeg trives i jobben min", fa
 #                     facet_color = "")
 #make_andel_bar_chart(df, yvalue = 'Jeg har følt at jeg har gått i mot normer og forventninger når det gjelder mine studie- og/eller karrierevalg',
 #                     facet_color = "")
-make_andel_bar_chart(df, yvalue = 'Jeg savner flere  kvinnelige tekniske rollemodeller i NAV IT', facet_color="")
+#make_andel_bar_chart(df, yvalue = 'Jeg savner flere  kvinnelige tekniske rollemodeller i NAV IT', facet_color="")
 #make_andel_bar_chart(df, yvalue = 'Jeg er fornøyd med kjønnsbalansen i avdelingen (Utvikling og Data)')
 #make_andel_bar_chart(df, yvalue = 'Det er mer vi bør gjøre for å utligne kjønnsforskjellene')
 
