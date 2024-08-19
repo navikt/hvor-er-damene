36de828b15e796364a60930261ed85a09b76f68b
caroliwi <caroline.winther@nav.no>
2024-08-19 21:31:06 +0200

    fikse på grafer og tall

--- pp_charts/ikt_so_chart.py
+++ pp_charts/ikt_so_chart.py
@@ -7,25 +7,28 @@ df = read_heda_bucket(bucket_name = 'samordna_opptak', file_name = 'samordna_opp
 
 
 df['Kvinneandel tilbud'] = round(df['Søkere tilbud Kvinne'] / (df['Søkere tilbud Total']) * 100,1)
+df['name']='Tilbud studieplass'
 
 minty='#43B6A5'
 lys_lilla = '#DEC3FF'
 mørk_lilla='#643999'
 
-fig = px.line(df, x="År", y="Kvinneandel tilbud",
+fig = px.line(df, x="År", y="Kvinneandel tilbud", color = "name",
               markers=True,
               color_discrete_sequence=[minty],
               #title="Samordna opptak IKT studier: Andel kvinner blant tilbud om studieplass ",
-              labels={"Kvinneandel tilbud": "%", "År": "", "variable": ""},
+              labels={"Kvinneandel tilbud": "%", "År": "", "name": ""},
               )
 fig.update_traces(marker={"size": 10})
 fig.update_traces(line={"width": 4})
 fig.update_layout(plot_bgcolor=mørk_lilla,
                   paper_bgcolor='rgba(0,0,0,0)',
-                  font_size=30,
+                  font_size=25,
                   font_color='white',
                   )
 fig.update_yaxes(range=[15,40])
+fig.update_layout(showlegend=True)
+fig.update_layout(legend=dict(orientation="h"))
 #fig.update_xaxes(range=[1960,2025])
 
 #fig.show()
--- pp_charts/kakediagram.py
+++ pp_charts/kakediagram.py
@@ -22,16 +22,18 @@ def kakediagram(kvinne_andel, mann_andel, tittel):
     )
     fig.update_traces(insidetextfont=dict(color='white'),
                       outsidetextfont=dict(color='white'))
+    fig.update_traces(showlegend=False, textinfo='none', )
     # save as svg
     fig.write_image(f"figurer_javazone/{tittel}.svg")
 
-kakediagram(30, 70, 'andel_IT')
+kakediagram(32, 68, 'andel_IT')
+kakediagram(19, 81, 'andel_u&d')
 kakediagram(15, 85, 'andel_utvikler')
 kakediagram(26, 74, 'andel_junior_utvikler')
 kakediagram(9, 81, 'andel_senior_utvikler')
 kakediagram(14, 86, 'andel_inkl_konsulent')
 kakediagram(8, 92, 'andel_security_champion')
-kakediagram(3, 97, 'andel_techlead')
+kakediagram(7, 93, 'andel_techlead')
 kakediagram(0, 100, 'andel_nais')
 kakediagram(0, 100, 'andel_teknologiprinsipal')
 
