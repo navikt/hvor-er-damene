07c85b961978deb70a483fb5ff2765dedf512006
Eric.Ludvigsen <67414562+ericludvigs@users.noreply.github.com>
2025-06-23 09:59:17 +0200

    Sommerstudenter: endre på metode som generer test data
    refactor til å kunne kjøre dry_run lokalt uten å være innom big query (som er treig) for å se hva som dukker opp i pandas
    
    fikset at kolonner i pandas alle fikk "quotes" rundt (med `skipinitialspace = True`)

--- hr_data/test_data_til_bq.py
+++ hr_data/test_data_til_bq.py
@@ -5,6 +5,7 @@ lager test-data direkte i pandas og laster opp til BigQuery test-tabell
 (prod vs dev environment?)
 """
 
+import csv
 import logging
 import sys
 
@@ -15,25 +16,53 @@ from google.cloud.bigquery import LoadJobConfig
 sys.path.append("..")
 from teamkatalogen_bq.funksjoner import create_client
 
-PROJECT_ID = "heda-prod-2664"
-SA_KEY_NAME = "heda-access-key"
-DATASET = "hr_data"
-TEST_TABLE_NAME = "test_ansatte_direktoratet"
 
-bq_client = create_client(PROJECT_ID, SA_KEY_NAME)
+def main(dry_run=False):
+    logging.basicConfig()
 
-df_test_mangfold_data = pd.read_csv("test_data_mock.csv", header=0, index_col=False, sep=";", parse_dates=[1])
-cols_test_mangfold_data = [col.strip() for col in df_test_mangfold_data.columns]
+    if dry_run:
+        logging.getLogger().setLevel(logging.INFO)
 
-logging.info(f"Generert test dataframe: \n{df_test_mangfold_data.head(10)}")
+    PROJECT_ID = "heda-prod-2664"
+    SA_KEY_NAME = "heda-access-key"
+    DATASET = "hr_data"
+    TEST_TABLE_NAME = "test_ansatte_direktoratet"
 
-# laste data til BQ
-job_config = LoadJobConfig(
-    write_disposition="WRITE_TRUNCATE",
-    create_disposition="CREATE_IF_NEEDED",
-)
-bq_dataset = f"{PROJECT_ID}.{DATASET}"
-bq_table = f"{bq_dataset}.{TEST_TABLE_NAME}"
+    bq_client = None
 
-run_job = bq_client.load_table_from_dataframe(df_test_mangfold_data, bq_table, job_config=job_config)
-run_job.result()
+    if not dry_run:
+        bq_client = create_client(PROJECT_ID, SA_KEY_NAME)
+
+    df_test_mangfold_data = pd.read_csv(
+        "test_data_mock.csv",
+        header=0,
+        index_col=False,
+        sep=";",
+        parse_dates=[1],
+        skipinitialspace=True,
+        engine="python",
+        quoting=csv.QUOTE_MINIMAL,
+    )
+    cols_test_mangfold_data = [col.strip() for col in df_test_mangfold_data.columns]
+
+    logging.info(f"Kolonner i dataframe som blir brukt: \n{cols_test_mangfold_data}")
+    logging.info(f"Generert test dataframe: \n{df_test_mangfold_data.head(10)}")
+
+    if not dry_run and bq_client:
+        # laste data til BQ
+        job_config = LoadJobConfig(
+            write_disposition="WRITE_TRUNCATE",
+            create_disposition="CREATE_IF_NEEDED",
+        )
+        bq_dataset = f"{PROJECT_ID}.{DATASET}"
+        bq_table = f"{bq_dataset}.{TEST_TABLE_NAME}"
+
+        run_job = bq_client.load_table_from_dataframe(df_test_mangfold_data, bq_table, job_config=job_config)
+        run_job.result()
+
+    return None
+
+
+if __name__ == "__main__":
+    dry_run = False
+    main(dry_run=dry_run)
