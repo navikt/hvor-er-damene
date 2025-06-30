c558e26c2a19639c5fa69c2a7fc41202e11c8163
Eric.Ludvigsen <67414562+ericludvigs@users.noreply.github.com>
2025-06-30 08:28:05 +0200

    Sommerstudenter: refaktor på daglig DAG kode så den kan speile 3 nye tabeller

--- hr_data/hr_data_til_bq.py
+++ hr_data/hr_data_til_bq.py
@@ -7,60 +7,89 @@ laster data fra Oracle onprem til BQ
 import json
 import logging
 import os
+import sys
 
 import oracledb
 import pandas as pd
 from google.cloud import secretmanager
-from google.cloud.bigquery import Client, LoadJobConfig
 
-logging.basicConfig(level=logging.INFO)
-
-sql = "select * from dt_hr.hrres_ressurs_sommer"
-
-# hente secret fra Google Secret Manager
-secret_name = "dvh_srv_team_heda_py"
-logging.info(f"Setter miljøvariabl-hemmeligheter fra: {secret_name}")
-full_secret_name = f"projects/847673271220/secrets/{secret_name}/versions/latest"
-client = secretmanager.SecretManagerServiceClient()
-response = client.access_secret_version(request={"name": full_secret_name})
-secret = json.loads(response.payload.data.decode("UTF-8"))
-
-os.environ["DB_HOST"] = secret["DB_HOST"]
-os.environ["DB_PORT"] = secret["DB_PORT"]
-os.environ["DB_USER"] = secret["DB_USER"]
-os.environ["DB_PASSWORD"] = secret["DB_PASSWORD"]
-os.environ["DB_SERVICE_NAME"] = secret["DB_SERVICE_NAME"]
-
-logging.info(
-    f"Secrets for {os.environ['DB_USER']} mot {os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_SERVICE_NAME']}"
-)
+sys.path.append("..")  # importere fra teamkatalogen_bq
+from hr_data.bigquery_funksjoner import bigquery_upload_hr_df
 
 
-# koble til Oracle og kjøre spørring
-connection = oracledb.connect(
-    port=os.environ["DB_PORT"],
-    host=os.environ["DB_HOST"],
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASSWORD"],
-    service_name=os.environ["DB_SERVICE_NAME"],
-)
+def hent_oracle_data_til_df(sql_query: str | None = None):
+    if sql_query is None:
+        raise ValueError("Must have an SQL query to execute against connected Oracle database")
 
-with connection.cursor() as cursor:
-    cursor.execute(sql)
-    columns = [col[0].lower() for col in cursor.description]
-    data = cursor.fetchall()
-df_ansatte = pd.DataFrame(data, columns=columns)
-logging.info(f"Hentet {len(df_ansatte)} rader")
-connection.close()
+    # hente secret fra Google Secret Manager
+    secret_name = "dvh_srv_team_heda_py"
+    logging.info(f"Setter miljøvariabl-hemmeligheter fra: {secret_name}")
+    full_secret_name = f"projects/847673271220/secrets/{secret_name}/versions/latest"
+    client = secretmanager.SecretManagerServiceClient()
+    response = client.access_secret_version(request={"name": full_secret_name})
+    secret = json.loads(response.payload.data.decode("UTF-8"))
 
-# laste data til BQ
-client = Client(project="heda-prod-2664")
-job_config = LoadJobConfig(
-    write_disposition="WRITE_TRUNCATE",
-    create_disposition="CREATE_IF_NEEDED",
-)
-bq_dataset = "heda-prod-2664.hr_data"
-bq_table = f"{bq_dataset}.ansatte_direktoratet_raw"
+    os.environ["DB_HOST"] = secret["DB_HOST"]
+    os.environ["DB_PORT"] = secret["DB_PORT"]
+    os.environ["DB_USER"] = secret["DB_USER"]
+    os.environ["DB_PASSWORD"] = secret["DB_PASSWORD"]
+    os.environ["DB_SERVICE_NAME"] = secret["DB_SERVICE_NAME"]
 
-run_job = client.load_table_from_dataframe(df_ansatte, bq_table, job_config=job_config)
-run_job.result()
+    logging.info(
+        f"Secrets for {os.environ['DB_USER']} mot {os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_SERVICE_NAME']}"
+    )
+
+    # koble til Oracle og kjøre spørring
+    connection = oracledb.connect(
+        port=os.environ["DB_PORT"],
+        host=os.environ["DB_HOST"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        service_name=os.environ["DB_SERVICE_NAME"],
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql_query)
+        columns = [col[0].lower() for col in cursor.description]
+        data = cursor.fetchall()
+    df_ansatte = pd.DataFrame(data, columns=columns)
+    logging.info(f"Hentet {len(df_ansatte)} rader")
+    connection.close()
+
+    return df_ansatte
+
+
+def main(project: str | None = None):
+    # maps a source oracle DB table to the target BigQuery table name to which it should be mirrored by daily uploads
+    oracle_source_to_target_bigquery_mapping_dict = {
+        "dt_hr.hrres_ressurs_sommer": "ansatte_direktoratet_raw",
+        "dt_hr.hrres_ressurs_sommer_tk": "ansatte_plus_teamkatalog_raw",
+        "dt_hr.hrres_ressurs_sommer_tk_rolle": "ansatte_plus_teamkatalog_rolle_raw",
+        "dt_hr.hrres_ressurs_sommer_tk_grupper": "ansatte_teamkatalog_grupper_raw",
+    }
+
+    bq_dataset = "hr_data"
+    sa_key_name = "heda-access-key"
+
+    for oracle_source_table, bq_target_table in oracle_source_to_target_bigquery_mapping_dict.items():
+        sql: str = f"select * from {oracle_source_table}"
+        # henter fra Oracle
+        df_ansatte = hent_oracle_data_til_df(sql_query=sql)
+        # sender data til BigQuery
+        bigquery_upload_hr_df(
+            hr_df=df_ansatte,
+            PROJECT_ID=project,
+            SA_KEY_NAME=sa_key_name,
+            DATASET=bq_dataset,
+            TABLE_NAME=bq_target_table,
+        )
+
+    return None
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    project = "heda-prod-2664"
+
+    main(project=project)
