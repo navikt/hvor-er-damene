b2296bebe0952494e8202d914d6bff12f718258e
caroliwi <caroline.winther@nav.no>
2025-07-04 10:58:08 +0200

    prøver å fikse dag som feiler - ikke bruke uv

--- dags/daglig_hr_data_til_bq.py
+++ dags/daglig_hr_data_til_bq.py
@@ -32,8 +32,8 @@ with DAG(
         repo="navikt/hvor-er-damene",
         script_path="hr_data/hr_data_til_bq.py",
         requirements_path="requirements_bq.txt",
-        use_uv_pip_install=True,
-        slack_channel="#team-heda",
+        #use_uv_pip_install=True,
+        #slack_channel="#team-heda",
         allowlist=allowlist,
     )
 
